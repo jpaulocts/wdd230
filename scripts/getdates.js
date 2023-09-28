7187343a69684e4089ffbd1b2abbbec7440d2812
jpaulocts <jpaulocts@gmail.com>
2023-09-27 21:06:25 -0300

    enhace homepage

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -4,3 +4,18 @@ document.querySelector('#year').textContent = new Date().toLocaleDateString('pt-
 
 const oLastModif = new Date(document.lastModified);
 document.querySelector('#lastModified').textContent = oLastModif;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+
+});
+
+document.querySelectorAll('.navigation').forEach(link => {
+    if(link.href === window.location.pathname) {
+        link.classList.add('active')
+    }
+} )
