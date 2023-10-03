1e9fb847d702f7955750fdcaaa0180e9fe0ad8ef
jpaulocts <jpaulocts@gmail.com>
2023-10-03 20:34:18 -0300

    adding menu.js

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -5,16 +5,6 @@ document.querySelector('#year').textContent = new Date().toLocaleDateString('pt-
 const oLastModif = new Date(document.lastModified);
 document.querySelector('#lastModified').textContent = oLastModif;
 
-const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
-
-hamButton.addEventListener('click', () => {
-	navigation.classList.toggle('open');
-	hamButton.classList.toggle('open');
-
-});
-
-
 const currentPageURL = window.location.href;
 
 const navLinks = document.querySelectorAll('nav a');
