3f05c1b78deee55b45c01153b3325cdac9f6066f
jpaulocts <jpaulocts@gmail.com>
2023-10-08 02:42:09 -0300

    adding chamber page

---
+++ chamber/scripts/fade-out.js
@@ -0,0 +1,27 @@
+var hero = document.getElementById('hero');
+
+function fadeOutOnScroll(element) {
+  if (!element) {
+    return;
+  }
+  
+  var distanceToTop = window.scrollY + element.getBoundingClientRect().top;
+  var elementHeight = element.offsetHeight;
+  var scrollTop = document.documentElement.scrollTop;
+  
+  var opacity = 1;
+  
+  if (scrollTop > distanceToTop) {
+    opacity = 1 - (scrollTop - distanceToTop) / elementHeight;
+  }
+  
+  if (opacity >= 0) {
+    element.style.opacity = opacity;
+  }
+}
+
+function scrollHandler() {
+  fadeOutOnScroll(hero);
+}
+
+window.addEventListener('scroll', scrollHandler);
