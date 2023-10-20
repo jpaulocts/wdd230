51c9e407741d76ac7d65d33c221cdb52592b97b4
jpaulocts <jpaulocts@gmail.com>
2023-10-20 01:19:21 -0300

    enhancing page

--- chamber/scripts/toggle-switch.js
+++ chamber/scripts/toggle-switch.js
@@ -1,20 +1,38 @@
 const buttonMode = document.querySelector("#mode");
 const main = document.querySelector("main");
+const header = document.querySelector(".header");
+const menu = document.querySelector("#menu");
+const nav = document.querySelector("nav");
+const a = document.querySelectorAll(".inactive");
 
 buttonMode.addEventListener("click", () => {
     if (document.querySelector("#checkbox").checked) {
      main.style.background = "#000000";
      main.style.color = "#FFFFFF";
-     document.querySelector("#textmode").innerText ="Dark";
+     header.style.background = "#14213D";
+     menu.style.color = "#FFFFFF";
+     nav.style.background = "#14213D";
+     for (let i=0; i< a.length; i++) 
+     {
+      a[i].style.color = "#FFFFFF";
+     }
      
      
+          
          
   } else {
 
      main.style.background = "#FFFFFF";
      main.style.color = "#000000";
-     document.querySelector("#textmode").innerText ="Light";
-       
+     header.style.background = "#FFFFFF";
+     menu.style.color = "#000000";
+     nav.style.background = "#FFFFFF"
+     for (let i=0; i< a.length; i++) 
+     {
+      a[i].style.color = "#000000";
+     }
+     
+
      
      
   }
