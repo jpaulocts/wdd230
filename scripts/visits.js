52df68fd520904e9bef88f4fa6049045000b6b28
jpaulocts <jpaulocts@gmail.com>
2023-10-27 02:57:32 -0300

    update home page

---
+++ scripts/visits.js
@@ -0,0 +1,13 @@
+const vistsDisplay = document.querySelector(".visits");
+
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) 
+{
+    vistsDisplay.textContent = numVisits;
+} else {
+    vistsDisplay.textContent = "This is your first visit. 😊 Welcome!"
+}
+
+numVisits++;
