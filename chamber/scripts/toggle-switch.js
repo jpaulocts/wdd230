7df21ffdfb33e898dd3a2212672d6844ed8f4683
jpaulocts <jpaulocts@gmail.com>
2023-11-03 12:44:34 -0300

    adjusting image ratio

--- chamber/scripts/toggle-switch.js
+++ chamber/scripts/toggle-switch.js
@@ -4,6 +4,7 @@ const header = document.querySelector(".header");
 const menu = document.querySelector("#menu");
 const nav = document.querySelector("nav");
 const a = document.querySelectorAll(".inactive");
+const h1NonHome = document.querySelector(".head-nohome");
 const width= 640;
 
 
@@ -12,6 +13,7 @@ buttonMode.addEventListener("click", () => {
     if (document.querySelector("#checkbox").checked) {
      main.style.background = "#000000";
      main.style.color = "#FFFFFF";
+     h1NonHome.style.color = "#FFFFFF";
      header.style.background = "#14213D";
      menu.style.color = "#FFFFFF";
      for (let i=0; i< a.length; i++) 
@@ -25,6 +27,7 @@ buttonMode.addEventListener("click", () => {
 
      main.style.background = "#FFFFFF";
      main.style.color = "#000000";
+     h1NonHome.style.color = "#000000"
      header.style.background = "#FFFFFF";
      menu.style.color = "#000000";
 
