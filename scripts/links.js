b1e9c772faa3b865573804f989868873ed16d06a
jpaulocts <jpaulocts@gmail.com>
2023-11-09 23:36:48 -0300

    update json file

--- scripts/links.js
+++ scripts/links.js
@@ -3,7 +3,7 @@ const linksURL = "https://jpaulocts.github.io/wdd230/data/links.json";
 
 async function getLinks() {
     const response = await fetch(linksURL);
-    const data = await response.jason();
+    const data = await response.json();
     console.log(data);
     
 }
