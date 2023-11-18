1f0b7f90e5b3e7170b7ed542cbf5ebee346e5f43
jpaulocts <jpaulocts@gmail.com>
2023-11-18 11:37:08 -0300

    day regulation

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -1,3 +1,5 @@
+const bannerDate = new Date();
+const weekday = bannerDate.getDay();
 const banner = document.querySelector("#banner");
 const closeSpan = document.querySelector("#close");
 
@@ -6,3 +8,9 @@ closeSpan.addEventListener("click", () => {
 
     banner.style.display = "none";
 } )
+
+
+if (weekday == 1 || weekday == 4 || weekday == 5 || weekday == 6){
+
+    banner.style.display = "none";
+}
